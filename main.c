ee54708cba10e5edfe53efaeac3624a35d914fe4
celikemir074-boop <celikemir074@gmail.com>
2025-11-27 18:01:20 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,50 +1,50 @@
 /*
  * Project: Smart Irrigation System - GPIO Register Level Driver
- * Author: Team Proposal
- * Description: Control of Solenoid Valve (Output) and Push Button (Input)
- * using direct Register Access (CMSIS).
+ * Hardware: STM32F407 Discovery Board
+ * Description: Control of Solenoid Valve (PD12) and Push Button (PA0)
+ * using direct Register Access.
  */
 
-#include "stm32f4xx.h"  // STM32F4 serisi için başlık
+#include "stm32f4xx.h"
 
 void GPIO_Config(void) {
     // 1. Clock Aktivasyonu (RCC AHB1 Register)
-    // GPIOA (Bit 0) ve GPIOC (Bit 2) clock sinyallerini aktif et.
+    // GPIOA (Buton için Bit 0) ve GPIOD (Valf/LED için Bit 3) clock aç.
     RCC->AHB1ENR |= (1UL << 0); // GPIOA Clock Enable
-    RCC->AHB1ENR |= (1UL << 2); // GPIOC Clock Enable
+    RCC->AHB1ENR |= (1UL << 3); // GPIOD Clock Enable
 
-    // 2. PA5'i Output (Çıkış) Olarak Ayarla (MODER Register)
-    // Pin 5 için bitler [11:10]. Output modu '01'dir.
-    GPIOA->MODER &= ~(3UL << (5 * 2));  // Önce temizle
-    GPIOA->MODER |= (1UL << (5 * 2));   // '01' yap (Output Mode)
+    // 2. PD12'yi Output (Çıkış) Olarak Ayarla (MODER Register)
+    // Pin 12 için bitler [25:24]. Output modu '01'dir.
+    GPIOD->MODER &= ~(3UL << (12 * 2));  // Temizle
+    GPIOD->MODER |= (1UL << (12 * 2));   // '01' yap (Output Mode)
 
-    // 3. PC13'ü Input (Giriş) Olarak Ayarla (MODER Register)
-    // Pin 13 için bitler [27:26]. Input modu '00'dır.
-    GPIOC->MODER &= ~(3UL << (13 * 2)); // Temizle -> '00' (Input Mode)
+    // 3. PA0'ı Input (Giriş) Olarak Ayarla (MODER Register)
+    // Pin 0 için bitler [1:0]. Input modu '00'dır.
+    GPIOA->MODER &= ~(3UL << (0 * 2));   // Temizle -> '00' (Input Mode)
     
-    // 4. Buton için Pull-up/Pull-down Ayarı (PUPDR)
-    GPIOC->PUPDR &= ~(3UL << (13 * 2)); // No pull-up, no pull-down
+    // 4. PA0 Butonu için Pull-down (Discovery'de gerekebilir ama genelde haricidir)
+    // "10" Pull-down mode yapar.
+    GPIOA->PUPDR &= ~(3UL << (0 * 2)); // Temizle
+    GPIOA->PUPDR |= (2UL << (0 * 2));  // Pull-down (Garanti olsun)
 }
 
 int main(void) {
-    // GPIO Ayarlarını Yükle
     GPIO_Config();
 
     while (1) {
-        // IDR (Input Data Register) okuma işlemi
-        // PC13 pininin durumunu kontrol et (Bit 13)
-        // Nucleo butonuna basılınca LOW (0) olur.
+        // IDR (Input Data Register) okuma
+        // STM32F407 Discovery üzerindeki Mavi buton basılınca "1" (HIGH) verir.
         
-        if (!(GPIOC->IDR & (1UL << 13))) { 
-            // BUTONA BASILDI: Valfi (PA5) Aç (HIGH)
-            GPIOA->ODR |= (1UL << 5); 
+        if (GPIOA->IDR & (1UL << 0)) { 
+            // BUTONA BASILDI: Valfi (PD12) Aç
+            GPIOD->ODR |= (1UL << 12); 
             
         } else {
-            // BUTON BIRAKILDI: Valfi (PA5) Kapat (LOW)
-            GPIOA->ODR &= ~(1UL << 5);
+            // BUTON BIRAKILDI: Valfi (PD12) Kapat
+            GPIOD->ODR &= ~(1UL << 12);
         }
         
-        // Basit bir gecikme (Debounce için)
+        // Debounce gecikmesi
         for (volatile int i = 0; i < 50000; i++);
     }
 }
